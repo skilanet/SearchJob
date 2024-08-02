b37648e23ec9affbd04e8b1ae8b8cc05baea6490
desska <desska@yandex.ru>
2024-08-03 09:55:57 +1000

    add navigation
    add fragments:
    
    fragment_search
    fragment_vacancy
    fragment_filter_settings
    fragment_filter_location
    fragment_filter_country
    fragment_filter_region
    fragment_filter_industry

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -1,14 +1,12 @@
 package ru.practicum.android.diploma.ui.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -18,7 +16,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -48,7 +45,6 @@ class SearchFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment SearchFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(
             param1: String,
