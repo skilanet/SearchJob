768f4ecd4fabf0bbde8e22af0c4e07cdc18cedbb
Aras432 <artem.a.baranov@yandex.ru>
2024-08-03 13:04:23 +0300

    исправляет замечания детекта еще раз

--- app/src/main/java/ru/practicum/android/diploma/data/network/impl/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/impl/RetrofitNetworkClient.kt
@@ -4,11 +4,11 @@ import ru.practicum.android.diploma.data.dto.Response
 import ru.practicum.android.diploma.data.network.HeadHunterApi
 import ru.practicum.android.diploma.data.network.NetworkClient
 
-class RetrofitNetworkClient(private val headHunterService: HeadHunterApi): NetworkClient {
+class RetrofitNetworkClient(private val headHunterService: HeadHunterApi) : NetworkClient {
     override suspend fun doRequest(dto: Any): Response {
         return Response(BAD_REQUEST)
     }
-    companion object{
+    companion object {
         const val BAD_REQUEST = 500
     }
 }
