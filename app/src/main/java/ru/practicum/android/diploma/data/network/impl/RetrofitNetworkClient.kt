1ef9a6da18fc620d71404736d4aaaf292e95481f
Artem <artem.a.baranov@yandex.ru>
2024-08-03 17:03:14 +0300

    Добавляет функцию проверки соединения

--- app/src/main/java/ru/practicum/android/diploma/data/network/impl/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/impl/RetrofitNetworkClient.kt
@@ -1,13 +1,31 @@
 package ru.practicum.android.diploma.data.network.impl
 
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import ru.practicum.android.diploma.data.dto.Response
 import ru.practicum.android.diploma.data.network.HeadHunterApi
 import ru.practicum.android.diploma.data.network.NetworkClient
 
-class RetrofitNetworkClient(private val headHunterService: HeadHunterApi) : NetworkClient {
+class RetrofitNetworkClient(
+    private val headHunterService: HeadHunterApi,
+    private val connectivityManager: ConnectivityManager
+) : NetworkClient {
     override suspend fun doRequest(dto: Any): Response {
         return Response(BAD_REQUEST)
     }
+
+    private fun isConnected(): Boolean {
+        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+            }
+        }
+        return false
+    }
+
     companion object {
         const val BAD_REQUEST = 500
     }
