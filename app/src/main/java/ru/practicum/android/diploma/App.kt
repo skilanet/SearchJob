18c8767d47d87a97e66cedc36b003ac9efa5368a
skilanet <filonuk05@gmail.com>
2024-08-04 17:13:00 +0700

    added koin initialize to app class

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -1,5 +1,14 @@
 package ru.practicum.android.diploma
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class App : Application() {}
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+        }
+    }
+}
