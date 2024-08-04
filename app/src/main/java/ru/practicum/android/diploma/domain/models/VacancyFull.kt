e458782540f50e78678e45309bcc1145c4bc510e
Artem <artem.a.baranov@yandex.ru>
2024-08-05 01:31:45 +0300

    создает model для вакансий

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/VacancyFull.kt
@@ -0,0 +1,19 @@
+package ru.practicum.android.diploma.domain.models
+
+data class VacancyFull(
+    val id: String,
+    val name: String = "",
+    val employerName: String = "",
+    val employerLogo90: String?,
+    val employerLogo240: String?,
+    val employerLogoOriginal: String?,
+    val salaryFrom: Int?,
+    val salaryTo: Int?,
+    val salaryCurrency: String?,
+    val area: String?,
+    val employment: String = "",
+    val schedule: String = "",
+    val experience: String = "",
+    val keySkills: ArrayList<String> = arrayListOf(),
+    val description: String = "",
+)
---
+++ app/src/main/java/ru/practicum/android/diploma/util/mappers/VacancyMapper.kt
@@ -0,0 +1,4 @@
+package ru.practicum.android.diploma.util.mappers
+
+class VacancyMapper {
+}
