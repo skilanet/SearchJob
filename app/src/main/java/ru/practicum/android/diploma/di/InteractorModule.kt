fb695f1b22e29ee617f2775db55eacf21c3c107f
Artem <artem.a.baranov@yandex.ru>
2024-08-09 15:26:05 +0300

    Создает FavoritesInteractor и добавляет его в Di

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,5 +1,9 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.domain.favorites.FavoritesInteractor
+import ru.practicum.android.diploma.domain.favorites.impl.FavoritesInteractorImpl
 
-val interactorModule = module { }
+val interactorModule = module {
+    factory<FavoritesInteractor> { FavoritesInteractorImpl(get()) }
+}
