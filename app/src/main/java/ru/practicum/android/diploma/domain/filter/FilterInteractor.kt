604f18cf0fe6f00121ea3d3f62c97e71cd342c3e
skilanet <filonuk05@gmail.com>
2024-08-18 23:41:44 +0700

    Добавлена функция получения стран в интерактор

--- app/src/main/java/ru/practicum/android/diploma/domain/filter/FilterInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/filter/FilterInteractor.kt
@@ -1,11 +1,13 @@
 package ru.practicum.android.diploma.domain.filter
 
+import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.filter.entity.Filter
 import ru.practicum.android.diploma.domain.filter.entity.FilterSetting
+import ru.practicum.android.diploma.domain.filter.entity.Resource
 
 interface FilterInteractor {
     fun saveSetting(setting: FilterSetting)
     fun getFilter(): Filter?
     fun isFilterPresent(): Boolean
-
+    fun getCountries(): Flow<Resource>
 }
--- app/src/main/java/ru/practicum/android/diploma/domain/filter/impl/FilterInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/filter/impl/FilterInteractorImpl.kt
@@ -1,9 +1,11 @@
 package ru.practicum.android.diploma.domain.filter.impl
 
+import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.filter.FilterInteractor
 import ru.practicum.android.diploma.domain.filter.FilterRepository
 import ru.practicum.android.diploma.domain.filter.entity.Filter
 import ru.practicum.android.diploma.domain.filter.entity.FilterSetting
+import ru.practicum.android.diploma.domain.filter.entity.Resource
 
 class FilterInteractorImpl(private val filterRepository: FilterRepository) : FilterInteractor {
     override fun saveSetting(setting: FilterSetting) {
@@ -16,5 +18,9 @@ class FilterInteractorImpl(private val filterRepository: FilterRepository) : Fil
 
     override fun isFilterPresent(): Boolean {
         return filterRepository.isFilterPresent()
+    }
+
+    override fun getCountries(): Flow<Resource> {
+        return filterRepository.getCountries()
     }
 }
