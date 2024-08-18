2fc3d80212f7d3da07cccb8bd9e9b68be9617519
desska <desska@yandex.ru>
2024-08-18 17:16:04 +1000

    Исправил ошибки detekt

--- app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterApi.kt
@@ -12,25 +12,30 @@ import ru.practicum.android.diploma.data.dto.VacanciesSearchResponse
 import ru.practicum.android.diploma.data.dto.VacancyDto
 
 interface HeadHunterApi {
-    @GET("/vacancies") suspend fun searchVacancies(
+    @GET("/vacancies")
+    suspend fun searchVacancies(
         @QueryMap params: Map<String, String>,
         @HeaderMap headers: Map<String, String>
     ): Response<VacanciesSearchResponse>
 
-    @GET("/vacancies/{vacancy_id}") suspend fun getVacancy(
+    @GET("/vacancies/{vacancy_id}")
+    suspend fun getVacancy(
         @Path("vacancy_id") id: String,
         @HeaderMap headers: Map<String, String>
     ): Response<VacancyDto>
 
-    @GET("/industries") suspend fun getIndustries(
+    @GET("/industries")
+    suspend fun getIndustries(
         @HeaderMap headers: Map<String, String>
     ): Response<List<IndustryParent>>
 
-    @GET("/areas") suspend fun getAreas(
+    @GET("/areas")
+    suspend fun getAreas(
         @HeaderMap headers: Map<String, String>
     ): Response<List<AreaParent>>
 
-    @GET("/areas/countries") suspend fun getCountries(
+    @GET("/areas/countries")
+    suspend fun getCountries(
         @HeaderMap headers: Map<String, String>
     ): Response<List<Area>>
 
--- app/src/main/java/ru/practicum/android/diploma/data/network/impl/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/impl/RetrofitNetworkClient.kt
@@ -157,9 +157,10 @@ class RetrofitNetworkClient(
     private fun isConnected(): Boolean {
         val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
         return capabilities?.run {
-            hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || hasTransport(
-                NetworkCapabilities.TRANSPORT_ETHERNET
-            )
+            hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+                || hasTransport(
+                    NetworkCapabilities.TRANSPORT_ETHERNET
+                )
         } ?: false
     }
 
