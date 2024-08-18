192f7ef25bd6218ae985dfe3ee527f34164f5f91
skilanet <filonuk05@gmail.com>
2024-08-18 23:41:17 +0700

    Создан файл ресурсов для стран

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/filter/entity/Resource.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.domain.filter.entity
+
+sealed interface Resource {
+    data class Success(val data: List<AreaEntity>): Resource
+    data class Error(val code: Int): Resource
+}
