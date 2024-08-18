694fea7de830be2df7c9b2b67ea5e66eb5866d4d
desska <desska@yandex.ru>
2024-08-18 12:54:40 +1000

    Сделал опциональными некоторые поля в Filter

--- app/src/main/java/ru/practicum/android/diploma/domain/filter/entity/FilterSetting.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/filter/entity/FilterSetting.kt
@@ -2,8 +2,8 @@ package ru.practicum.android.diploma.domain.filter.entity
 
 sealed class FilterSetting {
     data class Area(
-        val country: AreaEntity,
-        val region: AreaEntity
+        val country: AreaEntity?,
+        val region: AreaEntity?
     ) : FilterSetting()
 
     data class Industry(
@@ -12,7 +12,7 @@ sealed class FilterSetting {
     ) : FilterSetting()
 
     data class Salary(
-        val salary: Int,
-        val onlyWithSalary: Boolean
+        val salary: Int? = null,
+        val onlyWithSalary: Boolean? = null
     ) : FilterSetting()
 }
