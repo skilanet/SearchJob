6351055e75ebb07e814f116a60c6c96b58573856
desska <desska@yandex.ru>
2024-08-18 21:37:48 +1000

    Добавил ReferenceInfoRepository

---
+++ app/src/main/java/ru/practicum/android/diploma/data/referenceinfo/impl/ReferenceInfoRepositoryImpl.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.data.referenceinfo.impl
+
+import ru.practicum.android.diploma.data.network.NetworkClient
+import ru.practicum.android.diploma.domain.referenceinfo.ReferenceInfoRepository
+
+class ReferenceInfoRepositoryImpl(private val networkClient: NetworkClient) : ReferenceInfoRepository
