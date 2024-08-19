f27b2ebe7647e6c7702d9ce00436bab59a80bc0d
Artem <artem.a.baranov@yandex.ru>
2024-08-19 17:35:17 +0300

    Добавляет ViewModel для экрана фильтра по региону. Дополняет ReferenceInfoRepositoryImpl для сохранения страны региона. Добавляет в layout датабиндинги

--- app/src/main/java/ru/practicum/android/diploma/util/mappers/AreaMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/mappers/AreaMapper.kt
@@ -4,10 +4,14 @@ import ru.practicum.android.diploma.data.dto.AreaParent
 import ru.practicum.android.diploma.domain.filter.entity.AreaEntity
 
 class AreaMapper {
-    fun map(dto: AreaParent): AreaEntity {
+    fun map(dto: AreaParent, parent: AreaParent): AreaEntity {
         return AreaEntity(
             dto.id,
-            dto.name
+            dto.name,
+            AreaEntity(
+                parent.id,
+                parent.name
+            )
         )
     }
 }
