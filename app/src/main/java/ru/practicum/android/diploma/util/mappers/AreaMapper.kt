1b8fb30f5a10c48d3e1c7ec13837a622915bd3e5
skilanet <filonuk05@gmail.com>
2024-08-20 00:21:44 +0700

    detekt

--- app/src/main/java/ru/practicum/android/diploma/util/mappers/AreaMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/mappers/AreaMapper.kt
@@ -6,19 +6,12 @@ import ru.practicum.android.diploma.domain.filter.entity.AreaEntity
 
 class AreaMapper {
     fun map(dto: AreaParent, parent: AreaParent): AreaEntity {
-        return AreaEntity(
-                dto.id, dto.name, AreaEntity(
-                parent.id, parent.name
-        )
-        )
+        return AreaEntity(dto.id, dto.name, AreaEntity(parent.id, parent.name))
     }
 
     fun map(area: Area): AreaEntity {
         return with(area) {
-            AreaEntity(
-                    id = id!!, name = name!!
-            )
+            AreaEntity(id = id!!, name = name!!)
         }
     }
 }
-
