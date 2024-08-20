8b7116fbc51bd489c66817c5d5aaa2f28663542c
Artem <artem.a.baranov@yandex.ru>
2024-08-20 14:31:18 +0300

    фиксы дева

--- app/src/main/java/ru/practicum/android/diploma/ui/filtersettings/FilterSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtersettings/FilterSettingsFragment.kt
@@ -24,31 +24,4 @@ class FilterSettingsFragment : BindingFragment<FragmentFilterSettingsBinding>() 
         }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FilterSettingsFragment.
-         */
-        @JvmStatic
-        fun newInstance(
-            param1: String,
-            param2: String
-        ) =
-            FilterSettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(
-                        ARG_PARAM1,
-                        param1
-                    )
-                    putString(
-                        ARG_PARAM2,
-                        param2
-                    )
-                }
-            }
-    }
 }
